1b3e9ab4947f816efdbb22392ffd97423e18949c
Christian Emmer <christianemmer@stockx.com>
2019-03-20 13:49:58 -0400

    ldd *.

--- test.sh
+++ test.sh
@@ -2,29 +2,24 @@
 set -euxo pipefail
 
 
+# Ensure libtorrent-rasterbar.so exists
 LIBTORRENT_SO=$(find /usr/local/lib -name libtorrent-rasterbar.so*)
-
 if [[ "${LIBTORRENT_SO}" == "" ]]; then
     echo "Failed to find /usr/local/lib/libtorrent-rasterbar.so*" >&2
     exit 1
 fi
-
 echo "Found libtorrent:"
 echo "${LIBTORRENT_SO}"
 
-
-ldd /usr/local/lib/libtorrent-rasterbar.so*
-SHARED_SO=$($(for FILE in "${LIBTORRENT_SO}"; do ldd "${FILE}" | awk '{print $3}' | grep -v "^ldd$"; done) | uniq -u)
-
+# Ensure libtorrent-rasterbar.so dependencies exist
+SHARED_SO=$(ldd /usr/local/lib/libtorrent-rasterbar.so* | awk '{print $3}' | grep -v "^ldd$")
 for SO in "${SHARED_SO}"; do
     if [[ ! -e "${SO}" ]] ; then
         echo "Missing library file: ${LIB}" >&2
         exit 1
     fi
 done
-
 echo "Found required libraries:"
 echo "${SHARED_SO}"
-
 
 exit 0
